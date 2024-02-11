8f16e7f91ab285a89406996fdca7a193ee720aba
CHGRI4 <xgritsios@gmail.com>
2024-02-11 22:50:16 +0200

    -Partially made login page. -Made MainActivity

---
+++ app/src/main/java/com/example/virtualbussinessmobileapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.virtualbussinessmobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+        ImageView background = findViewById()
+    }
+
+}
