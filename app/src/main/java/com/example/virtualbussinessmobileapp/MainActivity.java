3ffffaaf28621695da83338e7ad6f5282ce967e9
CHGRI4 <xgritsios@gmail.com>
2024-02-19 19:47:25 +0200

    Made quite surely all the login_page.xml design Created new drawable round_button_shape.xml Tried to add animation

--- app/src/main/java/com/example/virtualbussinessmobileapp/MainActivity.java
+++ app/src/main/java/com/example/virtualbussinessmobileapp/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
+        /*Animation starting_input_anim = AnimationUtils.loadAnimation(this, R.anim.start_show);
+        starting_input_anim.reset();*/
+
     }
 
 }
